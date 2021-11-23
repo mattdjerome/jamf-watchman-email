6fcad2bf59651d117874ac537697064b3c2c9411
mattdjerome <mattdjerome@gmail.com>
2021-11-23 10:56:48 -0800

    fixed sys.argv bug

--- jamf_watchman_reference_email_public.py
+++ jamf_watchman_reference_email_public.py
@@ -3,26 +3,26 @@
 import requests
 import json
 import subprocess
-
+import sys
 
 ####
-# Uses JAMF enviro variables to take the email address from the 
-# computer name in jamf and puts into Watchman as the reference email. 
+# Uses JAMF enviro variables to take the email address from the
+# computer name in jamf and puts into Watchman as the reference email.
 # Currently the 'username' in the computer -> location is used but can be changed
-# 
+#
 # Written by Matt Jerome, IT Systems Administrator 11/22/2021
 ####
 
 ####
 # variables
 ####
-jamf_basic_auth = "$4"
+jamf_basic_auth = sys.argv[4]
 
-watchman_bearer = "$5"
+watchman_bearer = sys.argv[5]
 
-subdomain = "$6"
+subdomain = sys.argv[6]
 
-watchman_api = "$7"
+watchman_api = sys.argv[7]
 
 
 ####
@@ -38,10 +38,10 @@ def email_address(data):
 
 def get_jamf_email(computer_name,subdomain,jamf_basic_auth):
 	url = f"https://{subdomain}.jamfcloud.com/JSSResource/computers/name/{computer_name}"
-	
+
 	headers = {"Accept": "application/json",
 	"Authorization": f"Basic {jamf_basic_auth}"}
-	
+
 	response = requests.request("GET", url, headers=headers)
 	results = response.json()
 	email = None
@@ -53,16 +53,16 @@ def get_jamf_email(computer_name,subdomain,jamf_basic_auth):
 
 def get_watchman_data(subdomain,watchman_api,watchman_bearer):
 	url = f"https://{subdomain}.monitoringclient.com/v2.5/computers?page=1&api_key={watchman_api}"
-	
+
 	payload={}
 	headers = {
 	  'Authorization': f'Bearer {watchman_bearer}'
 	}
-	
+
 	response = requests.request("GET", url, headers=headers, data=payload)
 	results = response.json()
 	return results
-	
+
 
 def insert_reference_email(email,id,subdomain,watchman_api):
 	if email:
@@ -77,9 +77,9 @@ def insert_reference_email(email,id,subdomain,watchman_api):
 		response = requests.put(url, headers=headers, data=data)
 	else:
 		print("No valid Email")
-		
-		
-		
+
+
+
 ####
 # This is where stuff happens
 ####
@@ -97,6 +97,4 @@ for i in watchman_computer_data:
 		email_address = get_jamf_email(hostname,subdomain,jamf_basic_auth)
 		insert_reference_email(email_address, watchman_id,subdomain,watchman_api)
 		print("Assignment complete.")
-		break # exits loop completely 
-
-	
+		break # exits loop completely
